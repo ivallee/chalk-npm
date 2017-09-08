d5e7a24808df7e2c1b424f344ca5785ebb75800d
Isaac Vallee <isaacvallee@gmail.com>
2017-09-08 19:24:28 +0000

    chalk stuff

---
+++ index.js
@@ -0,0 +1,6 @@
+var chalk = require("chalk");
+
+var message = chalk.red("Check out this sweet ") +
+              chalk.blue("way of ") +
+              chalk.green("making funky coloured text, yo!");
+console.log(message);
